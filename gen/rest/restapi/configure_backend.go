888938a95bc4725224de132b093ac781078c43ea
avssvd <avssvd@yandex.ru>
2021-10-12 19:25:09 +0300

    Add swagger.yaml; generate server via go-swagger

---
+++ gen/rest/restapi/configure_backend.go
@@ -0,0 +1,90 @@
+// This file is safe to edit. Once it exists it will not be overwritten
+
+package restapi
+
+import (
+	"crypto/tls"
+	"net/http"
+
+	"github.com/go-openapi/errors"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/runtime/middleware"
+
+	"github.com/avssvd/ncr-test-golang/gen/rest/restapi/operations"
+)
+
+//go:generate swagger generate server --target ../../rest --name Backend --spec ../../../rest/swagger.yaml --principal interface{} --exclude-main
+
+func configureFlags(api *operations.BackendAPI) {
+	// api.CommandLineOptionsGroups = []swag.CommandLineOptionsGroup{ ... }
+}
+
+func configureAPI(api *operations.BackendAPI) http.Handler {
+	// configure the api here
+	api.ServeError = errors.ServeError
+
+	// Set your custom logger if needed. Default one is log.Printf
+	// Expected interface func(string, ...interface{})
+	//
+	// Example:
+	// api.Logger = log.Printf
+
+	api.UseSwaggerUI()
+	// To continue using redoc as your UI, uncomment the following line
+	// api.UseRedoc()
+
+	api.JSONConsumer = runtime.JSONConsumer()
+
+	api.JSONProducer = runtime.JSONProducer()
+
+	if api.DeleteControllerHandler == nil {
+		api.DeleteControllerHandler = operations.DeleteControllerHandlerFunc(func(params operations.DeleteControllerParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.DeleteController has not yet been implemented")
+		})
+	}
+	if api.GetControllerIndicationsHandler == nil {
+		api.GetControllerIndicationsHandler = operations.GetControllerIndicationsHandlerFunc(func(params operations.GetControllerIndicationsParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.GetControllerIndications has not yet been implemented")
+		})
+	}
+	if api.GetControllersHandler == nil {
+		api.GetControllersHandler = operations.GetControllersHandlerFunc(func(params operations.GetControllersParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.GetControllers has not yet been implemented")
+		})
+	}
+	if api.PostControllerHandler == nil {
+		api.PostControllerHandler = operations.PostControllerHandlerFunc(func(params operations.PostControllerParams) middleware.Responder {
+			return middleware.NotImplemented("operation operations.PostController has not yet been implemented")
+		})
+	}
+
+	api.PreServerShutdown = func() {}
+
+	api.ServerShutdown = func() {}
+
+	return setupGlobalMiddleware(api.Serve(setupMiddlewares))
+}
+
+// The TLS configuration before HTTPS server starts.
+func configureTLS(tlsConfig *tls.Config) {
+	// Make all necessary changes to the TLS configuration here.
+}
+
+// As soon as server is initialized but not run yet, this function will be called.
+// If you need to modify a config, store server instance to stop it individually later, this is the place.
+// This function can be called multiple times, depending on the number of serving schemes.
+// scheme value will be set accordingly: "http", "https" or "unix".
+func configureServer(s *http.Server, scheme, addr string) {
+}
+
+// The middleware configuration is for the handler executors. These do not apply to the swagger.json document.
+// The middleware executes after routing but before authentication, binding and validation.
+func setupMiddlewares(handler http.Handler) http.Handler {
+	return handler
+}
+
+// The middleware configuration happens before anything, this middleware also applies to serving the swagger.json document.
+// So this is a good place to plug in a panic handling middleware, logging and metrics.
+func setupGlobalMiddleware(handler http.Handler) http.Handler {
+	return handler
+}
